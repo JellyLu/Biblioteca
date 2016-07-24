f61784e4781da47b2583947529eb1ccd86c7db01
JellyLu <634725546@qq.com>
2016-07-24 19:29:28 +0800

    add Constrants for message

---
+++ src/main/java/tools/MessageConstants.java
@@ -0,0 +1,15 @@
+package tools;
+
+/**
+ * Created by yjlu@thoughtworks.com on 7/24/16.
+ */
+public final class MessageConstants {
+    public final String MSG_WELCOME = "Welcome to Biblioteca!";
+    public final String MSG_CHECKED_OUT_SUCCESSFUL = "That is not a valid book to return.";
+    public final String MSG_TURN_BOOK_SUCCESSFUL = "Thank you for returning the book.";
+
+    public final String ERR_INVALID_MENU_OPTION = "Select a valid option!";
+    public final String ERR_INVALID_BOOK_FOR_CHECKED_OUT = "That book is not available.";
+    public final String ERR_INVALID_BOOK_FOR_RETURN = "That is not a valid book to return.";
+
+}
