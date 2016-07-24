ef3fbb1ca2843b5466cf32ad3c64392734485058
JellyLu <634725546@qq.com>
2016-07-24 19:40:45 +0800

    add console log and input method

--- src/main/java/tools/ConsoleTool.java
+++ src/main/java/tools/ConsoleTool.java
@@ -1,7 +1,18 @@
 package tools;
 
+import java.util.Scanner;
+
 /**
  * Created by yjlu@thoughtworks.com on 7/24/16.
  */
 public class ConsoleTool {
+    public static void logln(String outPutString) {
+        System.out.println(outPutString);
+    }
+
+    public static String inputFromConsole() {
+        Scanner scan = new Scanner(System.in);
+
+        return scan.nextLine();
+    }
 }
