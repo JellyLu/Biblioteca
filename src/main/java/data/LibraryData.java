5ebbf3baf4905e88f3509966d6d93a278b76eb7d
JellyLu <634725546@qq.com>
2016-07-25 22:49:32 +0800

    add User test data

--- src/main/java/data/LibraryData.java
+++ src/main/java/data/LibraryData.java
@@ -2,6 +2,7 @@ package data;
 
 import model.Book;
 import model.Movie;
+import model.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +26,11 @@ public final class LibraryData {
         add(new Movie("M_3000", "The Lord of the Rings: The Return of the King", " Peter Jackson", 2003, 8.9));
         add(new Movie("M_4000", "Titanic", "James Cameron", 1997, 7.7));
         add(new Movie("M_5000", "Saving Private Ryan", "Steven Spielberg", 1998, 8.6));
+    }};
+
+    public final List<User> USER_LIST = new ArrayList<User>(){{
+        add(new User("xxx-0001", "Joy", "Test1234", "joy@thougtworks.com", "15881616205"));
+        add(new User("xxx-0002", "Jelly", "Test1234", "jelly@thougtworks.com", "15881616206"));
     }};
 }
 
